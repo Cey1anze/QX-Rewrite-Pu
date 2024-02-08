9b42828cb561aa217f9ccab600923018f6a92ad9
e1ectronic <2558985693@qq.com>
2024-02-08 20:11:29 +0800

    update

--- Pu.js
+++ Pu.js
@@ -49,6 +49,30 @@ if (body){
                 console.error("Error parsing JSON:", error);
             }
             break;
+        /**
+         * 主页精简
+         */
+        case /^https?:\/\/pocketuni.net\/api\/article\/index/.test($request.url):
+            try {
+                let data = JSON.parse(body);
+                if (data && data.content) {
+                    // 删除指定的键值对
+                    delete data.content.home_top_ad;
+                    delete data.content.place_ad_1;
+                    delete data.content.place_ad_2;
+                    delete data.content.place_ad_3;
+                    delete data.content.banner2;
+                    delete data.content.articles;
+                    delete data.content.entry2;
+                    // 删除 entry1 中的特定对象
+                    data.content.entry1 = data.content.entry1.filter(entry => entry.code !== "EventCalendar" && entry.code !== "more");
+                    // 修改完成后将数据转换回字符串
+                    body = JSON.stringify(data);
+                }
+            } catch (error) {
+                console.error("Error parsing JSON", error);
+            }
+            break;
         default:
             $done({})
     }
